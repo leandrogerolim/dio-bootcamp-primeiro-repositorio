f8bc711f8f012998f9605c7bd4b042da1f282062
Leandro <leandrogerolim@gmail.com>
2022-10-13 15:37:47 -0400

    Exercicio concluido.

--- loops-e-arrays/src/br/com/dio/exercicios/loops/Ex2_Nota.java
+++ loops-e-arrays/src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -4,24 +4,35 @@ import java.util.Scanner;
 
 /*
 Faça um programa que peça uma nota, entre zero e dez.
+
+
 Mostre uma mensagem caso o valor seja inválido
 e continue pedindo
+
 até que o usuário informe um valor válido.
 */
 public class Ex2_Nota {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         int nota;
+        System.out.print("Insira a nota No intervalo de 0 - 10");
+        nota = scanner.nextInt();
 
-        System.out.println("Nota: ");
-        nota = scan.nextInt();
 
-        while(nota < 0 | nota > 10) {
-            System.out.println("Nota Inválida! Digite novamente: ");
-            nota = scan.nextInt();
+        while(true) {
+            if (nota < 0 || nota > 10) {
+                System.out.println("Nota inválido insira a nota no intervalo de 0 - 10: ");
+                nota = scanner.nextInt();
+
+            } else {
+                System.out.println("A nota foi informada corretamente! Obrigado.");
+                break;
+
+            }
+
         }
+       System.out.println("A nota informada foi: " + nota);
 
     }
-
 }
