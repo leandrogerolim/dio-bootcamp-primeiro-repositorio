005fa471c5725719eabff6f8a835edc6e0785a43
Leandro <leandrogerolim@gmail.com>
2022-10-14 17:57:26 -0400

    Exercicio concluido.

--- loops-e-arrays/src/br/com/dio/exercicios/loops/Ex6_Fatorial.java
+++ loops-e-arrays/src/br/com/dio/exercicios/loops/Ex6_Fatorial.java
@@ -8,19 +8,21 @@ Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
 */
 public class Ex6_Fatorial {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Fatorial: ");
-        int fatorial = scan.nextInt();
+        System.out.println("Qual é o número fatorial: ");
+       int fatorial = scanner.nextInt();
 
-        int multiplicacao = 1;
+      int multiplicacao = 1;
 
-        System.out.print(fatorial +"! = ");
-        for(int i = fatorial ; i >= 1 ; i --) {
+
+        System.out.println("O número fatorial " + fatorial + " ! = ");
+        for(int i = fatorial; i >= 1; i--){
             multiplicacao = multiplicacao * i;
         }
-
         System.out.println(multiplicacao);
-    }
+
+
+       }
 
 }
