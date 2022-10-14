4be87b601db7bb51e3fd01fc4010654e2ee094e8
Leandro <leandrogerolim@gmail.com>
2022-10-14 18:14:16 -0400

    Exercicio concluido.

--- loops-e-arrays/src/br/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
+++ loops-e-arrays/src/br/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
@@ -5,22 +5,25 @@ e mostre-os na ordem inversa.
 */
 public class Ex1_OrdemInversa {
     public static void main(String[] args) {
+        int[] numeroInverso = {5, 2,8,61,88,48};
+        int contador = 0;
 
-        int[] vetor = {-5, -6, 15, 50, 8, 4};
 
-        //System.out.println(vetor.length);
 
-        System.out.print("Vetor: ");
-        int count =0;
-        while(count < (vetor.length)) {
-            System.out.print(vetor[count] + " ");
-            count++;
+        System.out.println(numeroInverso.length);
+        while(contador < numeroInverso.length){
+            System.out.println(numeroInverso[contador]);
+            contador++;
+
+        }
+        for (int i = (numeroInverso.length - 1); i >= 0; i--){
+            System.out.println(numeroInverso[i]);
+
         }
 
-        System.out.print("\nVetor: ");
-        for(int i = (vetor.length - 1); i >= 0; i --) {
-            System.out.print(vetor[i] + " ");
-        }
+
+
+
 
     }
 }
