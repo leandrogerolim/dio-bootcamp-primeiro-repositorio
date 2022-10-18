e220564484ed78193cc48b77265fb388b5baa158
Leandro <leandrogerolim@gmail.com>
2022-10-18 19:32:03 -0400

    Entrega do desafio FizzBuzz

---
+++ DesafioFizzBuzz/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+
+        int num = number.nextInt();
+        boolean fizz = num % 3 == 0;
+        boolean buzz = num % 5 == 0;
+        boolean fizzbuzz = fizz && buzz;
+
+
+        if (fizzbuzz == true ) {
+            System.out.println("FizzBuzz");
+
+
+        } else if
+
+        (buzz == true ) {
+            System.out.println("Buzz");
+
+        }else if(fizz == true) {
+            System.out.println("Fizz");
+
+        }else {
+
+
+            System.out.println(num);
+
+
+        }
+
+        // TODO: Retorne a palavra correta de acordo com o múltiplo de "num".
+        // Caso o valor não seja múltiplo de 3 ou 5, exiba o número, conforme o enunciado.
+
+    }
+}
+
+
