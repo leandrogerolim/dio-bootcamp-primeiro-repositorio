63e9e4ec67ac0da848ddbceb275e865733b93512
Leandro <leandrogerolim@gmail.com>
2022-10-20 20:38:04 -0400

    add classe curso e mentoria.

--- Desafio-dio-Poo/src/Main.java
+++ Desafio-dio-Poo/src/Main.java
@@ -1,5 +1,22 @@
+import desafio.dominio.Curso;
+import desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        Mentoria mentoria1 = new Mentoria();
+        curso1.setTitulo ("Curso java");
+        curso1.setDescricao ("Curso java muito bom");
+        curso1.setCargaHoraria (40);
+
+        mentoria1.setTitulo ("Mentoria de JAva");
+        mentoria1.setDescricao ("Mentoria JavaScript");
+        mentoria1.setData(LocalDate.now());
+        System.out.println(curso1);
+        System.out.println(mentoria1);
+
+
     }
 }
