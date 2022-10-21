78c33a233d47f093f2181ced5504ab9931e4e763
Leandro <leandrogerolim@gmail.com>
2022-10-20 21:00:09 -0400

    add classe BootCamp e Dev.

--- Desafio-dio-Poo/src/Main.java
+++ Desafio-dio-Poo/src/Main.java
@@ -1,3 +1,4 @@
+import desafio.dominio.Conteudo;
 import desafio.dominio.Curso;
 import desafio.dominio.Mentoria;
 
@@ -7,9 +8,13 @@ public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
         Mentoria mentoria1 = new Mentoria();
+        Conteudo conteudo1 = new Curso();
+
         curso1.setTitulo ("Curso java");
         curso1.setDescricao ("Curso java muito bom");
         curso1.setCargaHoraria (40);
+
+
 
         mentoria1.setTitulo ("Mentoria de JAva");
         mentoria1.setDescricao ("Mentoria JavaScript");
