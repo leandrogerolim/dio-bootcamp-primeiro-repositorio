ae6e246207509648fe1e82dd34f514c4fd9b2d27
Leandro <leandrogerolim@gmail.com>
2022-10-27 19:09:25 -0400

    desafio classificando matrizes criado

---
+++ DesafioClassificandoMatrizes/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+        int i;
+        int N = leitor.nextInt();
+        int [] nums = new int[N];
+        for (i = 0; i < N; i++)
+        {
+            nums[i] = leitor.nextInt();
+
+        }
+   -++++++++++++
+        //TODO: mova todos os inteiros pares para o inicio do array, seguido por todos os números ímpares
+   int j = 0;
+     //   for (i = 0; i < nums.length; i++) {
+     //       if (                   ) {
+      //          nums[i] = nums[i] ^ nums[j] ^ (nums[j] = nums[i]);
+//
+            }
+        }
+
+     //   for (var entry : nums) {
+
+ //       }
+//    }
+//}
