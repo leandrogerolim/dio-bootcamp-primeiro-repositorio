d1b867949acee982ce8af0d48cb6ea4d64600787
Leandro <leandrogerolim@gmail.com>
2022-10-29 11:01:30 -0400

    desafio classificando matrizes entregue.

--- DesafioClassificandoMatrizes/src/Main.java
+++ DesafioClassificandoMatrizes/src/Main.java
@@ -5,23 +5,47 @@ public class Main {
 
         Scanner leitor = new Scanner(System.in);
         int i;
+        int temporaria;
         int N = leitor.nextInt();
-        int [] nums = new int[N];
-        for (i = 0; i < N; i++)
-        {
+        int[] nums = new int[N];
+        for (i = 0; i < N; i++) {
             nums[i] = leitor.nextInt();
         }
         //TODO: mova todos os inteiros pares para o inicio do array, seguido por todos os números ímpares
-        int j = 0;
-        for (i = 0; i < nums.length; i++) {
-            if (                   ) {
-                nums[i] = nums[i] ^ nums[j] ^ (nums[j] = nums[i]);
 
+
+        int[] ordenacao = new int[N];
+
+        for (i = 0; i < N; i++)
+        if (nums[i] % 2 != 0) {
+
+            for (int j = (i + 1); j < N; j++) {
+                if (nums[j] % 2 == 0) {
+                    temporaria = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temporaria;
+                    j = N;
+
+                }
             }
-        }
 
-        for (var entry : nums) {
+//           for (i = 0; i < N; i++) {
+//               if (nums[i] % 2 != 0) {
+//                   for (int j = 0; j < N; j++) {
+//                       if (ordenacao[j] == 0) {
+//                           ordenacao[j] = nums[i];
+//                            break;
+//                       }
+//
+//                   }
+//              }
+//
+         }
+            for (i = 0;i< N; i++) {
+                System.out.println(nums[i]);
+            }
 
         }
     }
-}
+
+
